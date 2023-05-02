5155fa93ae6be2404466eb592157734133ec54b6
Abdullah H <71054105+abdullah50053@users.noreply.github.com>
2023-05-02 17:23:59 -0500

    Add colors and fonts

--- Game.py
+++ Game.py
@@ -12,6 +12,70 @@ class Game(tk.Frame):
         )
         self.main_grid.grid(pady=(110, 0))
 
+        # create GUI
+        # start game function
+
+        # key bindings
+        self.master.bind("<Left>", self.left)
+        self.master.bind("<Right>", self.right)
+        self.master.bind("<Up>", self.up)
+        self.master.bind("<Down>", self.down)
+
+        self.mainloop()
+
+
+    Color_EmptyCell = "#ffd5b5"
+    Font_ScoreLabel = ("Verdana", 24)
+    Font_Score = ("Helvetica", 48, "bold")
+    Font_GameOver = ("Helvetica", 48, "bold")
+    Font_Color_GameOver = "#ffffff"
+    Winner_BG = "#ffcc00"
+    Color_Background = "#a39489"
+    Color_FG = "#ffffff"
+ 
+    Color_Cells = {
+        2: "#fcefe6",
+        4: "#f2e8cb",
+        8: "#f5b682",
+        16: "#f29446",
+        32: "#ff775c",
+        64: "#e64c2e",
+        128: "#ede291",
+        256: "#fce130",
+        512: "#ffdb4a",
+        1024: "#f0b922",
+        2048: "#fad74d"    
+    }
+ 
+    Color_CellNumber = {
+        2: "#695c57",
+        4: "#695c57",
+        8: "#ffffff",
+        16: "#ffffff",
+        32: "#ffffff",
+        64: "#ffffff",
+        128: "#ffffff",
+        256: "#ffffff",
+        512: "#ffffff",
+        1024: "#ffffff",
+        2048: "#ffffff"
+    }
+ 
+    Fonts_CellNumber = {
+        2: ("Helvetica", 55, "bold"),
+        4: ("Helvetica", 55, "bold"),
+        8: ("Helvetica", 55, "bold"),
+        16: ("Helvetica", 50, "bold"),
+        32: ("Helvetica", 50, "bold"),
+        64: ("Helvetica", 50, "bold"),
+        128: ("Helvetica", 45, "bold"),
+        256: ("Helvetica", 45, "bold"),
+        512: ("Helvetica", 45, "bold"),
+        1024: ("Helvetica", 40, "bold"),
+        2048: ("Helvetica", 40, "bold"),
+    }
+
+
 def main():
     print()
 
